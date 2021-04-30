db03ef525173308aaad6721b657e66cd46502117
Sandrodro <sandrogach@gmail.com>
2021-05-01 01:52:35 +0400

    fix minor bugs

--- editData/editReview.php
+++ editData/editReview.php
@@ -15,16 +15,19 @@ foreach ($array_data as $item) {
     }
 }
 
+print_r($_POST['review']);
 //We add reviews as an array, so that each phone can have multiple reviews
 //We also try to guard against attacks by escaping the contents of the review
-$needed_phone['review'][] = htmlspecialchars(trim($_POST['review']));
+if (!empty($_POST['review'])) {
+    $needed_phone['review'][] = htmlspecialchars(trim($_POST['review']));
 
-foreach ($array_data as &$item) {
-    if ($item['id'] == "$id") {
-        $item = $needed_phone;
+    foreach ($array_data as &$item) {
+        if ($item['id'] == "$id") {
+            $item = $needed_phone;
+        }
     }
+
+    $newArr['phones'] = $array_data;
+
+    file_put_contents("../data/products.json", json_encode($newArr));
 }
-
-$newArr['phones'] = $array_data;
-
-file_put_contents("../data/products.json", json_encode($newArr));
--- product.js
+++ product.js
@@ -2,13 +2,18 @@ $("#submitReview").click(function() {
     let text = $("#review").val()
     // This gets the current phone's id from the url
     let id = window.location.href.split('=')[1]
-    $.ajax({
-        type: "POST",
-        url: "editData/editReview.php",
-        data: {
-            review: JSON.stringify(text),
-            id: id
-        }
-    })
-    location.reload()
+    if (id.includes("#")) {
+        id = id.slice(0, -1)
+    }
+    if (text) {
+        $.ajax({
+            type: "POST",
+            url: "editData/editReview.php",
+            data: {
+                review: JSON.stringify(text),
+                id: id
+            }
+        })
+        location.reload()
+    }
 })
