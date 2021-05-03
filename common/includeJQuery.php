b5829fe02c342f8f92589c1fbe519219409cdbc3
Lekso-Surameli <surameli2004@gmail.com>
2021-05-03 17:40:53 +0400

    delete unnecessary file

--- common/includeJQuery.php
+++
@@ -1,9 +0,0 @@
-  
-<?php 
-
-
-echo `<script
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-    src="https://code.jquery.com/jquery-3.6.0.min.js"
-    crossorigin="anonymous">
-    </script>;`;
