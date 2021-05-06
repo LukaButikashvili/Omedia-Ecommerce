dda946880ceb0ca3c6dec926288f9d2231c6d9dd
Sandrodro <sandrogach@gmail.com>
2021-05-07 00:04:22 +0400

    fix product add bug

--- editData/addData/upload-manager.php
+++ editData/addData/upload-manager.php
@@ -1,32 +1,32 @@
 <?php
 
-include_once "./common/addItemToData.php";
+include_once "../../common/addItemToData.php";
 
 
 // Check if the form was submitted
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if file was uploaded without errors
-    if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
+    if (isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0) {
         $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
         $filename = $_FILES["photo"]["name"];
         $filetype = $_FILES["photo"]["type"];
         $filesize = $_FILES["photo"]["size"];
-    
+
         // Verify file extension
         $ext = pathinfo($filename, PATHINFO_EXTENSION);
-        if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
-    
+        if (!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
+
         // Verify file size - 5MB maximum
         $maxsize = 5 * 1024 * 1024;
-        if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
-    
+        if ($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
+
         // Verify MYME type of the file
-        if(in_array($filetype, $allowed)){
+        if (in_array($filetype, $allowed)) {
             // Check whether file exists before uploading it
-            if(file_exists("images/" . $filename)){
+            if (file_exists("images/" . $filename)) {
                 echo $filename . " is already exists.";
-            } else{
+            } else {
                 move_uploaded_file($_FILES["photo"]["tmp_name"], "images/" . $filename);
                 echo "Your file was uploaded successfully.";
 
@@ -36,13 +36,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 $new_product_data['review'] = [];
 
                 //add product data to products.json
-                addItemToData('./data/products.json', $new_product_data, 'phones');
-            } 
-        } else{
-            echo "Error: There was a problem uploading your file. Please try again."; 
+                addItemToData('../../data/products.json', $new_product_data, 'phones');
+            }
+        } else {
+            echo "Error: There was a problem uploading your file. Please try again.";
         }
-    } else{
+    } else {
         echo "Error: " . $_FILES["photo"]["error"];
     }
 }
-?>
